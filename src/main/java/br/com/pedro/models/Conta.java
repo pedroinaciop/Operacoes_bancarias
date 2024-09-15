d92f8712b0527e9c8b84fc727991f4be1b06f30a
Pedro Inácio Penha dos Santos <123902574+pedroinaciop@users.noreply.github.com>
2024-09-15 16:06:14 -0300

    Update Conta.java

--- src/main/java/br/com/pedro/models/Conta.java
+++ src/main/java/br/com/pedro/models/Conta.java
@@ -1,2 +1,37 @@
-package br.com.pedro.models;public class Conta {
+package br.com.pedro.models;
+
+import java.math.BigDecimal;
+
+public class Conta {
+    private Integer numero;
+    private String senha;
+    private BigDecimal saldo;
+//    private boolean bloqueado;
+
+    public Conta(Integer numero, String senha, BigDecimal saldo) {
+        this.numero = numero;
+        this.senha = senha;
+        this.saldo = saldo;
+    }
+
+    public Integer getNumero() {
+        return this.numero;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public BigDecimal setSaldo(BigDecimal saldo) {
+        return this.saldo = saldo;
+    }
+
+    public BigDecimal getSaldo() {
+        return this.saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Número da conta: " + this.numero + ", Saldo: R$" + this.saldo;
+    }
 }
